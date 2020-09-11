04f1d3c9c63bbf8c5ae7d63194c999ec39bad892
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-09-10 18:08:53 -0700

    Updated track handling

--- stream_repeater/track.py
+++ stream_repeater/track.py
@@ -1,8 +1,20 @@
 """ Handling for individual tracks """
 
 class Track(object):
-    def __init__(self, artist, title, track_file, start_time):
+    def __init__(self, artist, title, track_file, index_time):
         self.artist = artist
-        self.start_time = start_time
+        self.index_time = index_time
         self.title = title
         self.track_file = track_file
+
+    def __artist(self, s):
+        self.artist = s
+
+    def __index_time(self, s):
+        self.index_time = s
+
+    def __title(self, s):
+        self.title = s
+
+    def __track_file(self, s):
+        self.track_file = s
