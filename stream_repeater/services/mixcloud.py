cae9bf94699e585ee950b413424176e9b3739398
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-09-15 19:23:43 -0700

    Added more parts to mixcloud

--- stream_repeater/services/mixcloud.py
+++ stream_repeater/services/mixcloud.py
@@ -1,8 +1,22 @@
 """ Handle requests involving Mixcloud """
-
 import requests
 
 class Mixcloud(object):
     """ Mixcloud-handling Object """
 
-    def __init__(self):
+    def __init__(self, token, stream, cuesheet):
+        self.base_url = 'https://api.mixcloud.com'
+        self.cuesheet = cuesheet
+        self.stream = stream
+        self.token = token
+
+    def upload(self):
+        """ Upload a mix to Mixcloud """
+
+        # Build the upload URL
+        url = self.base_url + "/upload/?access_token=" + self.token
+
+        payload = {
+            'mp3': self.stream.filename
+        }
+        requests.post(url, data=payload)
