96dacfadaab309f7c3c815321bcbe723ab846fc0
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-03-06 03:33:55 +0000

    Updated stream to store info in current_app

--- stream_repeater/stream/stream.py
+++ stream_repeater/stream/stream.py
@@ -19,18 +19,19 @@ class Stream(object):
         self.tags = options['stream']['tags']
         self.title = options['stream']['title']
 
+        self.cover_path = self.datadir + "/" + self.cover
+        self.file_path = self.datadir + "/" + self.filename
+        self.mp3_path = ''
+
     def convert_to_mp3(self):
         """ Convert a recording to MP3 """
 
         print("Converting to MP3")
 
-        stream_file_path = self.datadir + "/" + self.filename
-        cover_file_path = self.datadir + "/" + self.cover
-
-        recording = AudioSegment.from_wav(stream_file_path)
-        mp3_filename = os.path.splitext(stream_file_path)[0] + ".mp3"
+        recording = AudioSegment.from_wav(self.file_path)
+        self.mp3_path = os.path.splitext(self.file_path)[0] + ".mp3"
         recording.export(
-            mp3_filename,
+            self.mp3_path,
             bitrate=self.bitrate,
             format="mp3",
             tags={
@@ -38,7 +39,7 @@ class Stream(object):
                 'album': self.album,
                 'title': self.title
                 },
-            cover=cover_file_path
+            cover=self.cover_path
             )
             
-        return print("Converted " + stream_file_path + " to " + mp3_filename)
+        return print("Converted " + self.file_path + " to " + self.mp3_path)
