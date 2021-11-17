c7c35553fe46c93fa4964e044534f5977e1d6709
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-11-17 08:41:00 +0000

    Added checking for historysheet and cuesheet in Stream

--- stream_repeater/stream/stream.py
+++ stream_repeater/stream/stream.py
@@ -7,21 +7,31 @@ class Stream(object):
     """ Stream-handling Object """
 
     def __init__(self, options):
-        print("Stream initialized")
         self.album = options['stream']['album']
         self.bitrate = options['stream']['bitrate']
         self.cover = options['stream']['cover']
-        self.cuesheet = options['stream']['cuesheet']
         self.datadir = options['system']['datadir']
         self.filename = options['stream']['filename']
-        self.historysheet = options['stream']['historysheet']
         self.performer = options['stream']['performer']
         self.tags = options['stream']['tags']
         self.title = options['stream']['title']
 
+        # Check if a cuesheet was supplied
+        if 'cuesheet' in options['stream']:
+            self.cuesheet = options['stream']['cuesheet']
+        else:
+            self.cuesheet = ''
+
+        # Check if a historysheet was supplied
+        if 'historysheet' in options['stream']:
+            self.historysheet = options['stream']['historysheet']
+        else:
+            self.historysheet = ''
+
         self.cover_path = self.datadir + "/" + self.cover
         self.file_path = self.datadir + "/" + self.filename
         self.mp3_path = ''
+        print("Stream initialized")
 
     def convert_to_mp3(self):
         """ Convert a recording to MP3 """
