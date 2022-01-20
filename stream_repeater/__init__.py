dee240396e12f05fb2359f4cbe1dd2f15b0cac1d
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2022-01-20 05:58:39 +0000

    Stripped down to only functional services

--- stream_repeater/__init__.py
+++ stream_repeater/__init__.py
@@ -11,21 +11,11 @@ else:
     app.config.from_object("stream_repeater.config.DevConfig")
 
 import stream_repeater.views
-from stream_repeater.beatport import beatport
 from stream_repeater.mixcloud import mixcloud
-from stream_repeater.restream import restream
-from stream_repeater.spotify import spotify
 from stream_repeater.stream import stream
-from stream_repeater.telegram import telegram
-from stream_repeater.youtube import youtube
 
-app.register_blueprint(beatport, url_prefix='/beatport')
 app.register_blueprint(mixcloud, url_prefix='/mixcloud')
-app.register_blueprint(restream, url_prefix='/restream')
-app.register_blueprint(spotify, url_prefix='/spotify')
 app.register_blueprint(stream, url_prefix='/stream')
-app.register_blueprint(telegram, url_prefix='/telegram')
-app.register_blueprint(youtube, url_prefix='/youtube')
 
 if __name__ == "__main__":
     app.run()
